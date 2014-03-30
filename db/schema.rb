39af35148b3254eec4955d47920e8f40b702121d
talalc <talalchoudry@gmail.com>
2014-03-30 13:20:22 -0400

    created comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330050932) do
+ActiveRecord::Schema.define(version: 20140330171822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "mcomics", force: true do |t|
     t.integer  "mseries_id"
