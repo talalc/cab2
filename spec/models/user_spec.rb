fa34c763635928c5ffb314c61dd4dee31722afa8
talalc <talalchoudry@gmail.com>
2014-03-31 14:10:09 -0400

    fixed rspec user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,11 +12,12 @@ describe User do
   it { should validate_numericality_of(:cc).is_greater_than_or_equal_to(0) }
 
   it { should have_secure_password }
+  # it { should ensure_length_of(:password).is_at_least(8).is_at_most(16) }
   it { should validate_confirmation_of(:password) }
-  it { should ensure_length_of(:password).is_at_least(8).is_at_most(16) }
+
 
   it "must have a unique email" do
-    user = User.create(name: "talal", email: "talal@pizza.com", password: "abcd1234", password_confirmation: "abcd1234")
+    user = User.create(nick: "talal", email: "talal@pizza.com", password: "abcd1234", password_confirmation: "abcd1234")
     expect( user ).to validate_uniqueness_of(:email)
   end
 
