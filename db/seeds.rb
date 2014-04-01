52b4ece39667e5a3ff561048e27e5b846054e7bd
talalc <talalchoudry@gmail.com>
2014-04-01 17:31:20 -0400

    fixes #5 seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.new
+admin.email = "admin@admin.com"
+admin.nick = "admin"
+admin.password = "abcd1234"
+admin.password_confirmation = "abcd1234"
+admin.admin = true
+admin.save
+
+user = User.new
+user.email = "user@user.com"
+user.nick = "user"
+user.password = "password"
+user.password_confirmation = "password"
+user.save
+
+
