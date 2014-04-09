908b00f87fc36c9fdd7e390b1daa2bfc27dbe54a
talalc <talalchoudry@gmail.com>
2014-04-08 23:17:02 -0400

    css & partials

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,6 +7,7 @@ class SessionController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:notice] = "Logged in Successfully!"
       redirect_to root_path
     else
       flash[:notice] = "Invalid Credentials"
@@ -16,6 +17,7 @@ class SessionController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "Logged Out Successfully!"
     redirect_to root_path
   end
 
